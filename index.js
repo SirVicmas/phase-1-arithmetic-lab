066a0750a5adf5e6dd625b0234a54d2499366d8c
Masibo <victor.wafula@student.moringaschool.com>
2023-07-10 18:38:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 6
+let num4 = 16
+let mod = num4 % num3
+console.log(mod)
+
+let set =(1, 3, 9, 20)
+let max = Math.max(set)
+console.log(max)
